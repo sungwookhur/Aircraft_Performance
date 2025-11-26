5a1ee7d9b5b9763826b02102f09f45d01cd9ddd7
hur <swheo89@gmail.com>
2025-11-26 13:57:55 +0900

    Proj02_build

---
+++ Proj02/build/CMakeFiles/Proj02.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Proj02.
+# This may be replaced when dependencies are built.
---
+++ Proj02/src/proj02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    double a = 1.0;
+    double b = 2.0;
+    double res = a + b; 
+    
+    std::cout << res;
+
+    return 0;
+}
